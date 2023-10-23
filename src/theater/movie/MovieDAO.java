b82a73c1887e46a9a495b2f08dcd44c24040d673
PJ0609 <PJ0609 pjy06091@gmail.com>
2023-10-23 12:03:17 +0900

    -

--- src/theater/movie/MovieDAO.java
+++ src/theater/movie/MovieDAO.java
@@ -22,7 +22,8 @@ public class MovieDAO {
 	
 	private final String INSERT_MOVIE = "insert into movie(mov_name, director, mov_img, genre, rating, synopsis, length, rel_date, trailer_link) values(?,?,?,?,?,?,?,?,?)";
 	private final String UPDATE_MOVIE = "update movie set mov_name=?, director=?, mov_img=?, genre=?, rating=?, synopsis=?, length=?, rel_date=?, trailer_link=? where mov_id=?";
-	
+	private final String DELETE_MOVIE = "delete movie where mov_id=?"; 
+	// 영화 추가
 	public int insertMovie(MovieDTO movie) {
 		int chk = 0;
 		try {
@@ -45,7 +46,8 @@ public class MovieDAO {
 		}
 		return chk;
 	}
-
+	
+	// 영화 변경
 	public int updateMovie(MovieDTO movie) {
 		int chk = 0;
 		try {
@@ -70,8 +72,23 @@ public class MovieDAO {
 		return chk;
 	}
 	
-	// 영화 삭제
-	public int deleteMovie(List<String> mov_id_array) {
+	// 영화 삭제 (복수)
+		public int deleteMovie(String mov_id) {
+			int chk = 0;
+			try {
+				conn = JDBCUtil.getConnection();
+				pstmt = conn.prepareStatement(DELETE_MOVIE);
+				pstmt.setString(1, mov_id);
+				chk = pstmt.executeUpdate();
+			} catch(Exception e) {
+				e.printStackTrace();
+			} finally {
+				JDBCUtil.close(conn, pstmt, rs);
+			}
+			return chk;
+		}
+	// 영화 삭제 (복수)
+	public int deleteMovie(String[] mov_id_array) {
 		int chk = 0;
 		String mov_ids = String.join(",", mov_id_array);
 		try {
